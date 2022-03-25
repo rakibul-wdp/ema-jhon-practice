cce1400927fc3637e1d55399891655b17e7fb4be
rakibul-wdp <rakibul.wdp@gmail.com>
2022-03-25 08:31:46 +0600

    added cart component

---
+++ src/components/Product/Product.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import './Product.css';
+
+const Product = ({product, handleAddToCart}) => {
+  const { img, name, seller, price, ratings } = product;
+  return (
+    <div className='product'>
+      <img src={img} alt='' />
+      <div className='product-info'>
+        <p className='product-name'>{name}</p>
+        <p>Price: ${price}</p>
+        <p>
+          <small>Seller: {seller}</small>
+        </p>
+        <p>
+          <small>Ratings: {ratings}</small>
+        </p>
+      </div>
+      <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+        <p className='btn-text'>Add to Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+      </button>
+    </div>
+  );
+};
+
+export default Product;
