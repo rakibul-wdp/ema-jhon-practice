64110a9ab204dfc10688ba5c65bc17f46bf2c2b1
rakibul-wdp <rakibul.wdp@gmail.com>
2022-04-15 04:11:11 +0600

    after long time

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>Let's gonna be know us...!!!</h2>
+      <p>Although we are secret!</p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import useProducts from '../../hooks/useProducts';
 import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
+
+  const [products, setProducts] = useProducts();
   const [cart, setCart] = useState([]);
-  useEffect(() => {
-    fetch('products.json')
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
 
   useEffect( () => {
     const storedCart = getStoredCart();
@@ -49,7 +47,11 @@ const Shop = () => {
         ))}
       </div>
       <div className='cart-container'>
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart}>
+          <Link to='/orders'>
+            <button>Review Order</button>
+          </Link>
+        </Cart>
       </div>
     </div>
   );
