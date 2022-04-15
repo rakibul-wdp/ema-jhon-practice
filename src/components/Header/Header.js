7ae5ee5ea6090b3f3fc8b9750205c05e23c40daf
rakibul-wdp <rakibul.wdp@gmail.com>
2022-04-15 12:46:03 +0600

    implement sign out and create shipment form

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './Header.css';
-import logo from '../../images/Logo.svg'
+import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
   const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  }
   return (
     <nav className='header'>
-      <img src={logo} alt="" />
+      <img src={logo} alt='' />
       <div>
-        <Link to="/shop">Shop</Link>
-        <Link to="/orders">Orders</Link>
-        <Link to="/inventory">Inventory</Link>
-        <Link to="/about">About</Link>
-        {user ? <button>Sign OUt</button> : <Link to='/login'>Login</Link>}
+        <Link to='/shop'>Shop</Link>
+        <Link to='/orders'>Orders</Link>
+        <Link to='/inventory'>Inventory</Link>
+        <Link to='/about'>About</Link>
+        {user ? <button onClick={handleSignOut}>Sign OUt</button> : <Link to='/login'>Login</Link>}
       </div>
     </nav>
   );
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -27,7 +27,7 @@ const Orders = () => {
       </div>
       <div className='cart-container'>
         <Cart cart={cart}>
-          <button onClick={() => navigate('/inventory')}>Proceed checkout</button>
+          <button onClick={() => navigate('/shipment')}>Proceed Shipping</button>
         </Cart>
       </div>
     </div>
