bd273ffcdb373a1bccb63f5429fc3b39ea88d836
rakibul-wdp <rakibul.wdp@gmail.com>
2022-04-15 10:19:22 +0600

    install react-firebase-hook and use it

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,10 +1,13 @@
 /**
- * 1. create a new project in console.google.com
+ * 1. create a new firebase project in console.firebase.google.com
  * 2. npm install firebase
  * 3. create firebase.init.js and import getAuth to export auth
  * 4. Firebase settings > Authentication > enable Email and Password auth
  * 5. Create Login, Signup component setup route
  * 6. attach form field handler and form submit handler
+ * 7. npm install --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword from react-firebase-hooks
+ * 9. 
 */
 
 /**
